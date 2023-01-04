01a91dbf07925289d7a7fad42a077f46d24377d4
Siddharth1207 <siddharth.dureja12@gmail.com>
2023-01-04 03:40:21 -0500

    remove_all

--- src/main/java/com/javalearning/transactions/repo/PaymentInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javalearning.transactions.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.javalearning.transactions.entity.PaymentInfo;
-
-public interface PaymentInfoRepository extends JpaRepository<PaymentInfo, String> {
-
-}
