77d501abfff275f27bdb097a79e1f86c69b1e2d9
Siddharth1207 <siddharth.dureja12@gmail.com>
2023-01-04 03:35:12 -0500

    remove all

--- src/main/java/com/javalearning/transactions/exception/InsufficientAmount.java
+++
@@ -1,9 +0,0 @@
-package com.javalearning.transactions.exception;
-
-public class InsufficientAmount extends RuntimeException {
-
-    public InsufficientAmount(String arg0) {
-        super(arg0);
-    }
-
-}
